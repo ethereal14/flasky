69cd389b7ba31c60d1f947a0814e0c7e426715ff
ethereal14 <1097282916@qq.com>
2025-08-11 23:25:58 +0800

    Chapter 2: A complete application (2a)

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Hello, World!</h1>'
+
 def main():
     print("Hello from flasky!")
+    app.run(debug=True)
 
 
 if __name__ == "__main__":
